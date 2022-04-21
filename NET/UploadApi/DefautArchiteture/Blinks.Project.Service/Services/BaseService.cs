956e97d66dc64a6f97eb66770341a91a7138b194
Rafael Soares De Britto <rafael.britto@essenceglobal.com>
2022-04-21 20:28:24 -0300

    removido camada service para validacoes

--- NET/UploadApi/DefautArchiteture/Blinks.Project.Service/Services/BaseService.cs
+++ NET/UploadApi/DefautArchiteture/Blinks.Project.Service/Services/BaseService.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Blinks.Project.Service.Services
 {
+    [Obsolete("Não precisamos mais usar Service para comunicar com o banco, toda validação de campos deve ser feita na camada de Application")]
     public class BaseService<TEntity> : IBaseService<TEntity> where TEntity : BaseEntity
     {
         private readonly IBaseRepository<TEntity> _baseRepository;
